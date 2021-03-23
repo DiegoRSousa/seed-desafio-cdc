5d7703c296521889ac90c3a5bbaa09da9d10dd49
Diego Sousa <diegors90@gmail.com>
2021-03-23 13:23:46 -0300

    adicinado validacao de email duplicado

--- src/main/java/br/com/diego/seeddesafiocdc/AutorController.java
+++ src/main/java/br/com/diego/seeddesafiocdc/AutorController.java
@@ -5,6 +5,9 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,13 @@ public class AutorController {
 	//1
 	@Autowired
 	private AutorRepository autorRepository;
+	@Autowired
+	private ProibeEmailDuplicadoAutorValidator proibeEmailDuplicadoAutorValidator;
+	
+	@InitBinder
+	public void init(WebDataBinder binder) {
+		binder.addValidators(proibeEmailDuplicadoAutorValidator);
+	}
 	//1
 	//1
 	@PostMapping
